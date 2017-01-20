62afacee099d2ac5b5724e1e1ab914ece918cbc9
masmowa <masmowa@gmail.com>
2017-01-19 17:53:22 -0800

    using shorter test that uses subtraction

--- Challenges/Sherlock-Array/Sherlock-Array/Program.cs
+++ Challenges/Sherlock-Array/Sherlock-Array/Program.cs
@@ -20,6 +20,17 @@ namespace Sherlock_Array
             {
                 int N = Convert.ToInt32(Console.ReadLine());
                 int[] A = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+#if true
+                if (testEqual(N, A))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+
+#else
                 if (N == 1)
                 {
                     Console.WriteLine("YES");
@@ -74,7 +85,7 @@ namespace Sherlock_Array
                     }
 
                 }
-
+#endif
             }
             //if (IsDebug)
             //{
@@ -87,6 +98,30 @@ namespace Sherlock_Array
             int nexti = i + 1;
             int nextj = j - 1;
             return (nexti == nextj);
+        }
+        static bool testEqual(int N, int[] A)
+        {
+            int lsum = 0;
+            int rsum = 0;
+            int sum = 0;
+            bool found = false;
+            for (int i = 0; i < N; ++i)
+            {
+                sum += A[i];
+            }
+            rsum = sum;
+            for (int i=0; i < N; i++)
+            {
+                lsum = sum - lsum;
+                rsum -= A[i];
+                if (rsum == lsum)
+                {
+                    found = true;
+                    break;
+                }
+
+            }
+            return found;
         }
     }
 }
